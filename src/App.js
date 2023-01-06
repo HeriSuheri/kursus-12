7f43e7a4d91fb8cf76d98e408858bcd7ce74202c
HERY SUHERY <herysuhery94@gmail.com>
2023-01-06 11:46:21 +0700

    title baru

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   const [listTitle, setListTitle] = useState('My List');
 
   const changeTitleHandler = useCallback(() => {
-    setListTitle('New Title');
+    setListTitle('New Title Baru');
   }, []);
 
   const listItems = useMemo(() => [ 12,14,30,33, 35], []);
